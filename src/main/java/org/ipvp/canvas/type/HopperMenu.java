16ab9fefa7f50e69a1d70f98f6da2200ee6c0c8f
Matthew <matthew.steglinski@utoronto.ca>
2017-05-13 09:50:36 -0400

    Add Hopper menu

---
+++ src/main/java/org/ipvp/canvas/type/HopperMenu.java
@@ -0,0 +1,37 @@
+package org.ipvp.canvas.type;
+
+import org.bukkit.event.inventory.InventoryType;
+import org.ipvp.canvas.Menu;
+
+/**
+ * A menu backed by a Hopper Inventory.
+ */
+public class HopperMenu extends AbstractMenu {
+
+    HopperMenu(String title, Menu menu) {
+        super(title, InventoryType.HOPPER, menu);
+    }
+
+    /**
+     * Returns a new builder. The Menu generated will be backed by a Hopper inventory.
+     */
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    @Override
+    public Dimension getDimensions() {
+        return new Dimension(1, 5);
+    }
+
+    /**
+     * A builder for creating a BoxMenu instance.
+     */
+    public static class Builder extends AbstractMenu.Builder {
+
+        @Override
+        public HopperMenu build() {
+            return new HopperMenu(getTitle(), getParent());
+        }
+    }
+}
