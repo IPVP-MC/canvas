2086e241af9f29092e95b7370823c8bb47170bea
Alexander <protonull@protonmail.com>
2021-04-21 01:15:59 +0100

    Update AbstractMenu.java
    Moves the removal of the MenuHolder below the close handler invocation, allowing for logic upon on the menu contents, such as, returning items placed within the inventory to the player upon close.

--- src/main/java/org/ipvp/canvas/type/AbstractMenu.java
+++ src/main/java/org/ipvp/canvas/type/AbstractMenu.java
@@ -184,11 +184,12 @@ public abstract class AbstractMenu implements Menu  {
             return;
         }
 
-        MenuHolder holder = (MenuHolder) currentInventory;
-        viewers.remove(holder);
         if (triggerCloseHandler) {
             getCloseHandler().ifPresent(h -> h.close(viewer, this));
         }
+        
+        MenuHolder holder = (MenuHolder) currentInventory;
+        viewers.remove(holder);
     }
 
     public Set<MenuHolder> getViewers() {
